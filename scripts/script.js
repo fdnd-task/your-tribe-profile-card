b9d301f8c920a2e0186233ccae9d339b8ccbbc5c
MathijsN <72303334+MathijsN@users.noreply.github.com>
2025-09-01 14:44:48 +0200

    Changed content and styles

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,7 @@
-/* Stap 1: Selecteer de knop */
-let expandButton = document.querySelector('.profile-card-example-button')
+const expandButton = document.querySelector('.profile-card-example-button')
 
-/* Stap 2: Wacht tot iemand op die knop klikt */
 expandButton?.addEventListener('click', function() {
-
-  /* Stap 3: Verander een class zodra iemand op die knop klikt */
   document.body.classList.toggle('is-expanded')
 })
+
+
