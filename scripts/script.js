6d60e2eff889da118b6daf2f9b95a10efccae7c6
Koop Reynders <k.g.reynders@hva.nl>
2026-08-29 09:12:54 +0200

    CSS (en JS) simpeler gemaakt

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 /* Stap 1: Selecteer de knop */
-let expandButton = document.querySelector('.profile-card-example-button')
+let expandButton = document.querySelector('button')
 
 /* Stap 2: Wacht tot iemand op die knop klikt */
-expandButton?.addEventListener('click', function() {
+expandButton.addEventListener('click', function() {
 
   /* Stap 3: Verander een class zodra iemand op die knop klikt */
   document.body.classList.toggle('is-expanded')
